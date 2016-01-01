13afe4345049d483374043f226f9e3026e66f3e7
neelo <neelo_sim@hotmail.com>
2016-01-01 17:20:13 +0000

    Added proper handling of filenames with spaces.

--- .i3/wallpaper_cycler.sh
+++ .i3/wallpaper_cycler.sh
@@ -5,15 +5,19 @@ WALLPAPERDIR="$HOME/Pictures/Wallpapers"
 DELAY=300
 
 trap ':' USR1 # Send a SIGUSR1 to this process to skip to the next wallpaper
+trap 'break' USR2 # Send a SIGUSR2 to this process to reload the wallpapers
+		  # and restart the loop
+
+IFS=$'\n'
 
 while true
 do
-	FILES="$WALLPAPERDIR"/*
+	FILES=($(find "$WALLPAPERDIR" -type f))
 	if [[ "$SHUFFLE" ]]
 	then
-		FILES=$(shuf -e $FILES)
+		FILES=($(shuf -e ${FILES[@]}))
 	fi
-	for WALLPAPER in $FILES
+	for WALLPAPER in ${FILES[@]}
 	do
 		feh --bg-max "$WALLPAPER"
 		sleep $DELAY &
