3529bc1f2152b3ab3495e25e0807fd66ec184bdb
Tomás Cunha <tomas.g.cunha@outlook.com>
2024-02-21 03:43:04 +0000

    Update harpoon keybindings

--- nvim/.config/nvim/lua/neelo/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/neelo/plugins/harpoon.lua
@@ -17,9 +17,9 @@ return {
       { "<leader>hv", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end },
 
       { "<C-h>",      function() harpoon:list():select(1) end },
-      { "<C-t>",      function() harpoon:list():select(2) end },
-      { "<C-n>",      function() harpoon:list():select(3) end },
-      { "<C-s>",      function() harpoon:list():select(3) end },
+      { "<C-j>",      function() harpoon:list():select(2) end },
+      { "<C-k>",      function() harpoon:list():select(3) end },
+      { "<C-l>",      function() harpoon:list():select(4) end },
 
       { "<C-S-P>",    function() harpoon:list():prev() end },
       { "<C-S-N>",    function() harpoon:list():next() end },
