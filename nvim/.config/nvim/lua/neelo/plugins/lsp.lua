d5ac04703c8e32ee98aee81cf99ecfed887952a7
Tomás Cunha <tomas.g.cunha@outlook.com>
2024-03-08 13:04:33 +0000

    Add which-key plugin

--- nvim/.config/nvim/lua/neelo/plugins/lsp.lua
+++ nvim/.config/nvim/lua/neelo/plugins/lsp.lua
@@ -10,23 +10,25 @@ return {
       lsp_zero.extend_lspconfig()
 
       lsp_zero.on_attach(function(_, buffnr)
-        local opts = { buffer = buffnr, remap = false }
+        local opts = function(desc)
+          return { desc = desc, buffer = buffnr, remap = false }
+        end
 
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Definition"))
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts("Implementation"))
+        vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts("References"))
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Documentation"))
 
-        vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-        vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+        vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts("Workspace symbols"))
+        vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts("Diagnostics"))
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts("Next diagnostic"))
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts("Previous diagnostic"))
 
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "<leader>vs", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts("Rename"))
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts("Code actions"))
+        vim.keymap.set("n", "<leader>vs", vim.lsp.buf.signature_help, opts("Signature help"))
 
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, opts)
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, opts("Format"))
 
         lsp_zero.buffer_autoformat()
       end)
