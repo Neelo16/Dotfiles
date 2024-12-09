246d3302d3c5cd530f4a2ee3386d83c798be6217
Tomás Cunha <tomas.g.cunha@outlook.com>
2024-12-09 02:19:38 +0000

    Add CopilotChat

--- nvim/.config/nvim/lua/neelo/plugins/copilot.lua
+++ nvim/.config/nvim/lua/neelo/plugins/copilot.lua
@@ -1,3 +1,46 @@
 return {
   'github/copilot.vim',
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-telescope/telescope.nvim" },
+      { "github/copilot.vim" },                              -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim",        branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken",                                 -- Only on MacOS or Linux
+    cmd = {
+      "CopilotChat",
+      "CopilotChatOpen",
+      "CopilotChatClose",
+      "CopilotChatToggle",
+      "CopilotChatLoad",
+      "CopilotChatModels",
+      "CopilotChatAgents",
+      "CopilotChatExplain",
+      "CopilotChatReview",
+      "CopilotChatFix",
+      "CopilotChatOptimize",
+      "CopilotChatDocs",
+      "CopilotChatTests",
+      "CopilotChatCommit",
+    },
+    opts = {
+      highlight_headers = false,
+      separator = '---',
+      error_header = '> [!ERROR] Error',
+    },
+    keys = function()
+      return {
+        {
+          "<leader>ccp",
+          function()
+            local actions = require("CopilotChat.actions")
+            require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+          end,
+          desc = "Copilot project files",
+          mode = { "n", "v" },
+        },
+      }
+    end,
+  },
 }
