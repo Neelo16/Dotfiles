4a0e3a6e292ddd81cd4ec63c295949ac20383fc2
Tomás Cunha <tomas.g.cunha@outlook.com>
2024-12-24 19:28:24 +0000

    Add snacks

--- nvim/.config/nvim/lua/neelo/plugins/snacks.lua
+++ nvim/.config/nvim/lua/neelo/plugins/snacks.lua
@@ -2,21 +2,23 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  ---@type snacks.Config
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
     bigfile = { enabled = true },
     dashboard = { enabled = true },
-    indent = { enabled = true },
+    indent = {
+      animate = { enabled = false },
+    },
     lazygit = { enabled = true },
     gitbrowser = { enabled = true },
     input = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
-    scroll = { enabled = true },
-    -- statuscolumn = { enabled = true },
+    scope = { enabled = true },
+    toggle = { enabled = true },
+    statuscolumn = { enabled = true },
     words = { enabled = true },
   },
+  keys = {
+    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+  }
 }
