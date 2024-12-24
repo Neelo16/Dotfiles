1dd37dd49a8aa41bfe04d370a98ee78fa167a5aa
Tomás Cunha <tomas.g.cunha@outlook.com>
2024-12-24 19:43:35 +0000

    Add telescope-dap

--- nvim/.config/nvim/lua/neelo/plugins/dap.lua
+++ nvim/.config/nvim/lua/neelo/plugins/dap.lua
@@ -2,6 +2,7 @@ return {
   "rcarriga/nvim-dap-ui",
   dependencies = {
     "mfussenegger/nvim-dap",
-    "nvim-neotest/nvim-nio"
+    "nvim-neotest/nvim-nio",
+    "nvim-telescope/telescope-dap.nvim",
   }
 }
