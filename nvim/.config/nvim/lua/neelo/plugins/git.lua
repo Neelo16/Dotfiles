3c495c2be8c38c474758ec6f89d0b07d66835205
Tomás Cunha <tomas.cunha@1global.com>
2025-01-14 18:16:42 +0000

    Add gitsigns

--- nvim/.config/nvim/lua/neelo/plugins/git.lua
+++ nvim/.config/nvim/lua/neelo/plugins/git.lua
@@ -5,5 +5,11 @@ return {
     keys = {
       { "<leader>gs", vim.cmd.Git, desc = "Git status" },
     },
-  }
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function(_, opts)
+      require("gitsigns").setup(opts)
+    end,
+  },
 }
