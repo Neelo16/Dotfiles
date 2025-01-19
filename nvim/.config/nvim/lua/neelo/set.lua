8e9b152c803e59903fb33a4f1381a230dc63ce74
Tomás Cunha <tomas.g.cunha@outlook.com>
2025-01-19 02:30:23 +0000

    Change indentation to 4 spaces

--- nvim/.config/nvim/lua/neelo/set.lua
+++ nvim/.config/nvim/lua/neelo/set.lua
@@ -1,9 +1,9 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
