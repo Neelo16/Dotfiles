63c1f25be7a0102456d8f371a113f4f1053ff349
Tomás Cunha <tomas.cunha@1global.com>
2025-03-18 15:36:26 +0000

    Add vim-dadbod

---
+++ nvim/.config/nvim/lua/neelo/plugins/dadbod.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod',                     lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+      { 'hrsh7th/nvim-cmp' },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+
+      local cmp = require('cmp')
+      cmp.setup.filetype({ 'sql', 'mysql', 'plsql' }, {
+        sources = {
+          { name = 'vim-dadbod-completion' },
+          { name = 'buffer' },
+        }
+      })
+    end,
+  },
+}
