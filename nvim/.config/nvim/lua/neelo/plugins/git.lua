dee4dbd32f9f4399aca77b5660f7e5d0ddd78e4e
Tomás Cunha <tomas.cunha@1global.com>
2025-03-26 10:37:31 +0000

    Add git-link

--- nvim/.config/nvim/lua/neelo/plugins/git.lua
+++ nvim/.config/nvim/lua/neelo/plugins/git.lua
@@ -12,4 +12,22 @@ return {
       require("gitsigns").setup(opts)
     end,
   },
+  {
+    "juacker/git-link.nvim",
+    lazy = true,
+    keys = {
+      {
+        "<leader>gu",
+        function() require("git-link.main").copy_line_url() end,
+        desc = "Copy code URL to clipboard",
+        mode = { "n", "x" }
+      },
+      {
+        "<leader>go",
+        function() require("git-link.main").open_line_url() end,
+        desc = "Open code URL in browser",
+        mode = { "n", "x" }
+      },
+    },
+  }
 }
